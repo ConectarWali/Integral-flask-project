4fa55fe01451eec7da28dd0b620c3cf65504284d
Conectar Wali SAS <167815566+ConectarWali@users.noreply.github.com>
2025-01-13 21:55:15 -0500

    Update __init__.py

--- integral_flask_project/__init__.py
+++ integral_flask_project/__init__.py
@@ -3,5 +3,5 @@ from integral_flask_project.integral_project import Integral_flask_project
 from src_IFP.config.config import Config, Development_config, Production_config
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __all__ = ['Integral_flask_project', 'import_moduls', 'Development_config', 'Production_config', 'Config']
